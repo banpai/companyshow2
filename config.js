252b215d3f891de931d59e72e392835f16340288
banpai <banpai@yeah.net>
2017-09-04 17:33:15 +0800

    添加我的咨询和提交我的信息的接口

--- config.js
+++ config.js
@@ -7,19 +7,19 @@
 var host = "https://api.shanggao.wshoto.com/app/index.php?"
 
 var config = {
-    // 下面的地址配合云端 Server 工作
+    //下面的地址配合云端 Server 工作
     host,
     //主页显示
     index2_api:`${host}i=5&c=entry&do=index2_api&m=siyuan_cms`,
     //idea2_api 留言板提交
     idea2_api:`${host}i=5&c=entry&do=idea2_api&m=siyuan_cms`,
-    // list2_api点击更多
+    //list2_api点击更多
     list2_api:`${host}i=5&c=entry&do=list2_api&m=siyuan_cms`,
-    // engine2_api工程案例
+    //engine2_api工程案例
     engine2_api:`${host}i=5&c=entry&do=engine2_api&m=siyuan_cms`,
-    // goods2_api产品展示
+    //goods2_api产品展示
     goods2_api:`${host}i=5&c=entry&do=goods2_api&m=siyuan_cms`,
-    // news2_api新闻
+    //news2_api新闻
     news2_api:`${host}i=5&c=entry&do=news2_api&m=siyuan_cms`,
     //addquestion2_api咨询提交
     addquestion2_api:`${host}i=5&c=entry&do=addquestion2_api&m=siyuan_cms`,
